2c05104c5622cc8cdd406d5bfaae9e1ff3156add
scoombe <skatesam95@gmail.com>
2016-08-08 22:18:02 +0100

    Tom and Sam Project
    adding needed files and a html skeleton

---
+++ index.py
@@ -0,0 +1,11 @@
+print (
+"<html>"+
+    "<head>"
+        "<script src='jquery-3.1.0.min.js'></script>"+
+        "<script src='script.js'><script>"+
+        "< link rel = 'stylesheet type = 'text/css' href =style.css"
+    "</head>"+
+    "<body>"+
+
+    "</body>"+
+"</html>")
